7665989935760737550895d4ba5f6c4b82783417
jorge <jribgomez@gmail.com>
2019-10-21 14:11:32 +0200

    Añadir formulario preguntas

---
+++ AppDesktop/Form2.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace AppDesktop
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxIdioma = new System.Windows.Forms.ComboBox();
+            this.labelIdioma = new System.Windows.Forms.Label();
+            this.buttonGuardarYSalir = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelTitol = new System.Windows.Forms.Label();
+            this.checkBoxCorrecta = new System.Windows.Forms.CheckBox();
+            this.buttonAyuda = new System.Windows.Forms.Button();
+            this.textBoxResposta = new System.Windows.Forms.TextBox();
+            this.textBoxPregunta = new System.Windows.Forms.TextBox();
+            this.comboBoxNivel = new System.Windows.Forms.ComboBox();
+            this.buttonReiniciar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            this.buttonModificar = new System.Windows.Forms.Button();
+            this.buttonAnadir = new System.Windows.Forms.Button();
+            this.buttonValidar = new System.Windows.Forms.Button();
+            this.labelResposta = new System.Windows.Forms.Label();
+            this.labelPregunta = new System.Windows.Forms.Label();
+            this.labelNivel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxIdioma
+            // 
+            this.comboBoxIdioma.FormattingEnabled = true;
+            this.comboBoxIdioma.Location = new System.Drawing.Point(70, 67);
+            this.comboBoxIdioma.Name = "comboBoxIdioma";
+            this.comboBoxIdioma.Size = new System.Drawing.Size(143, 21);
+            this.comboBoxIdioma.TabIndex = 54;
+            // 
+            // labelIdioma
+            // 
+            this.labelIdioma.AutoSize = true;
+            this.labelIdioma.Location = new System.Drawing.Point(9, 70);
+            this.labelIdioma.Name = "labelIdioma";
+            this.labelIdioma.Size = new System.Drawing.Size(38, 13);
+            this.labelIdioma.TabIndex = 53;
+            this.labelIdioma.Text = "Idioma";
+            // 
+            // buttonGuardarYSalir
+            // 
+            this.buttonGuardarYSalir.Location = new System.Drawing.Point(392, 362);
+            this.buttonGuardarYSalir.Name = "buttonGuardarYSalir";
+            this.buttonGuardarYSalir.Size = new System.Drawing.Size(108, 23);
+            this.buttonGuardarYSalir.TabIndex = 52;
+            this.buttonGuardarYSalir.Text = "Guardar";
+            this.buttonGuardarYSalir.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(487, 158);
+            this.dataGridView1.TabIndex = 51;
+            // 
+            // labelTitol
+            // 
+            this.labelTitol.AutoSize = true;
+            this.labelTitol.Location = new System.Drawing.Point(67, 15);
+            this.labelTitol.Name = "labelTitol";
+            this.labelTitol.Size = new System.Drawing.Size(109, 13);
+            this.labelTitol.TabIndex = 50;
+            this.labelTitol.Text = "AFEGIR PREGUNTA";
+            // 
+            // checkBoxCorrecta
+            // 
+            this.checkBoxCorrecta.AutoSize = true;
+            this.checkBoxCorrecta.Location = new System.Drawing.Point(484, 164);
+            this.checkBoxCorrecta.Name = "checkBoxCorrecta";
+            this.checkBoxCorrecta.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxCorrecta.TabIndex = 49;
+            this.checkBoxCorrecta.UseVisualStyleBackColor = true;
+            // 
+            // buttonAyuda
+            // 
+            this.buttonAyuda.Location = new System.Drawing.Point(512, 15);
+            this.buttonAyuda.Name = "buttonAyuda";
+            this.buttonAyuda.Size = new System.Drawing.Size(75, 23);
+            this.buttonAyuda.TabIndex = 48;
+            this.buttonAyuda.Text = "Ajuda";
+            this.buttonAyuda.UseVisualStyleBackColor = true;
+            // 
+            // textBoxResposta
+            // 
+            this.textBoxResposta.Location = new System.Drawing.Point(70, 161);
+            this.textBoxResposta.Name = "textBoxResposta";
+            this.textBoxResposta.Size = new System.Drawing.Size(395, 20);
+            this.textBoxResposta.TabIndex = 47;
+            // 
+            // textBoxPregunta
+            // 
+            this.textBoxPregunta.Location = new System.Drawing.Point(70, 114);
+            this.textBoxPregunta.Multiline = true;
+            this.textBoxPregunta.Name = "textBoxPregunta";
+            this.textBoxPregunta.Size = new System.Drawing.Size(417, 20);
+            this.textBoxPregunta.TabIndex = 46;
+            // 
+            // comboBoxNivel
+            // 
+            this.comboBoxNivel.FormattingEnabled = true;
+            this.comboBoxNivel.Location = new System.Drawing.Point(428, 67);
+            this.comboBoxNivel.Name = "comboBoxNivel";
+            this.comboBoxNivel.Size = new System.Drawing.Size(159, 21);
+            this.comboBoxNivel.TabIndex = 45;
+            // 
+            // buttonReiniciar
+            // 
+            this.buttonReiniciar.Location = new System.Drawing.Point(283, 362);
+            this.buttonReiniciar.Name = "buttonReiniciar";
+            this.buttonReiniciar.Size = new System.Drawing.Size(101, 23);
+            this.buttonReiniciar.TabIndex = 44;
+            this.buttonReiniciar.Text = "Reiniciar";
+            this.buttonReiniciar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.Location = new System.Drawing.Point(512, 251);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(75, 23);
+            this.buttonEliminar.TabIndex = 43;
+            this.buttonEliminar.Text = "Eliminar";
+            this.buttonEliminar.UseVisualStyleBackColor = true;
+            // 
+            // buttonModificar
+            // 
+            this.buttonModificar.Location = new System.Drawing.Point(512, 222);
+            this.buttonModificar.Name = "buttonModificar";
+            this.buttonModificar.Size = new System.Drawing.Size(75, 23);
+            this.buttonModificar.TabIndex = 42;
+            this.buttonModificar.Text = "Modificar";
+            this.buttonModificar.UseVisualStyleBackColor = true;
+            // 
+            // buttonAnadir
+            // 
+            this.buttonAnadir.Location = new System.Drawing.Point(512, 159);
+            this.buttonAnadir.Name = "buttonAnadir";
+            this.buttonAnadir.Size = new System.Drawing.Size(75, 23);
+            this.buttonAnadir.TabIndex = 41;
+            this.buttonAnadir.Text = "Afegir";
+            this.buttonAnadir.UseVisualStyleBackColor = true;
+            // 
+            // buttonValidar
+            // 
+            this.buttonValidar.Location = new System.Drawing.Point(512, 112);
+            this.buttonValidar.Name = "buttonValidar";
+            this.buttonValidar.Size = new System.Drawing.Size(75, 23);
+            this.buttonValidar.TabIndex = 40;
+            this.buttonValidar.Text = "Validar";
+            this.buttonValidar.UseVisualStyleBackColor = true;
+            // 
+            // labelResposta
+            // 
+            this.labelResposta.AutoSize = true;
+            this.labelResposta.Location = new System.Drawing.Point(9, 164);
+            this.labelResposta.Name = "labelResposta";
+            this.labelResposta.Size = new System.Drawing.Size(52, 13);
+            this.labelResposta.TabIndex = 39;
+            this.labelResposta.Text = "Resposta";
+            // 
+            // labelPregunta
+            // 
+            this.labelPregunta.AutoSize = true;
+            this.labelPregunta.Location = new System.Drawing.Point(9, 117);
+            this.labelPregunta.Name = "labelPregunta";
+            this.labelPregunta.Size = new System.Drawing.Size(50, 13);
+            this.labelPregunta.TabIndex = 38;
+            this.labelPregunta.Text = "Pregunta";
+            // 
+            // labelNivel
+            // 
+            this.labelNivel.AutoSize = true;
+            this.labelNivel.Location = new System.Drawing.Point(367, 70);
+            this.labelNivel.Name = "labelNivel";
+            this.labelNivel.Size = new System.Drawing.Size(33, 13);
+            this.labelNivel.TabIndex = 37;
+            this.labelNivel.Text = "Nivell";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 450);
+            this.Controls.Add(this.comboBoxIdioma);
+            this.Controls.Add(this.labelIdioma);
+            this.Controls.Add(this.buttonGuardarYSalir);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelTitol);
+            this.Controls.Add(this.checkBoxCorrecta);
+            this.Controls.Add(this.buttonAyuda);
+            this.Controls.Add(this.textBoxResposta);
+            this.Controls.Add(this.textBoxPregunta);
+            this.Controls.Add(this.comboBoxNivel);
+            this.Controls.Add(this.buttonReiniciar);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonModificar);
+            this.Controls.Add(this.buttonAnadir);
+            this.Controls.Add(this.buttonValidar);
+            this.Controls.Add(this.labelResposta);
+            this.Controls.Add(this.labelPregunta);
+            this.Controls.Add(this.labelNivel);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxIdioma;
+        private System.Windows.Forms.Label labelIdioma;
+        private System.Windows.Forms.Button buttonGuardarYSalir;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelTitol;
+        private System.Windows.Forms.CheckBox checkBoxCorrecta;
+        private System.Windows.Forms.Button buttonAyuda;
+        private System.Windows.Forms.TextBox textBoxResposta;
+        private System.Windows.Forms.TextBox textBoxPregunta;
+        private System.Windows.Forms.ComboBox comboBoxNivel;
+        private System.Windows.Forms.Button buttonReiniciar;
+        private System.Windows.Forms.Button buttonEliminar;
+        private System.Windows.Forms.Button buttonModificar;
+        private System.Windows.Forms.Button buttonAnadir;
+        private System.Windows.Forms.Button buttonValidar;
+        private System.Windows.Forms.Label labelResposta;
+        private System.Windows.Forms.Label labelPregunta;
+        private System.Windows.Forms.Label labelNivel;
+    }
+}
