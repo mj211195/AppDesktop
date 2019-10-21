db4021d3c402c01d3f05dcc1f13f1c3333d41c53
María José Benites Gaitán <mariajosebg95@gmail.com>
2019-10-21 12:53:54 +0200

    Agregar nivel

--- AppDesktop/GestionPreguntas.Designer.cs
+++ AppDesktop/GestionPreguntas.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Nivel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // Nivel
+            // 
+            this.Nivel.AutoSize = true;
+            this.Nivel.Location = new System.Drawing.Point(23, 42);
+            this.Nivel.Name = "Nivel";
+            this.Nivel.Size = new System.Drawing.Size(33, 13);
+            this.Nivel.TabIndex = 0;
+            this.Nivel.Text = "Nivell";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(74, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(147, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // GestionPreguntas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "GestionPreguntas";
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Nivel);
+            this.Name = "GestionPreguntas";
+            this.Text = "Gestió de preguntes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Nivel;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
