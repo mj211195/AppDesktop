afa67717d27bdc4ee36f8fd99218a5ad52e92b11
Juan <carlos25840@gmail.com>
2019-10-22 09:25:44 +0200

    Form1 con logo

--- AppDesktop/Form1.Designer.cs
+++ AppDesktop/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.roundedButtonGestionar = new AppDesktop.RoundedButton();
+            this.roundedButtonSuport = new AppDesktop.RoundedButton();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // roundedButtonGestionar
+            // 
+            this.roundedButtonGestionar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButtonGestionar.Location = new System.Drawing.Point(130, 191);
+            this.roundedButtonGestionar.Name = "roundedButtonGestionar";
+            this.roundedButtonGestionar.Size = new System.Drawing.Size(92, 35);
+            this.roundedButtonGestionar.TabIndex = 0;
+            this.roundedButtonGestionar.Text = "Gestionar";
+            this.roundedButtonGestionar.UseVisualStyleBackColor = true;
+            // 
+            // roundedButtonSuport
+            // 
+            this.roundedButtonSuport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roundedButtonSuport.Location = new System.Drawing.Point(130, 242);
+            this.roundedButtonSuport.Name = "roundedButtonSuport";
+            this.roundedButtonSuport.Size = new System.Drawing.Size(92, 35);
+            this.roundedButtonSuport.TabIndex = 1;
+            this.roundedButtonSuport.Text = "Suport";
+            this.roundedButtonSuport.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Image = global::AppDesktop.Properties.Resources.Logo_Museu;
+            this.pictureBoxLogo.Location = new System.Drawing.Point(112, 24);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(139, 115);
+            this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxLogo.TabIndex = 2;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(347, 359);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.roundedButtonSuport);
+            this.Controls.Add(this.roundedButtonGestionar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private RoundedButton roundedButtonGestionar;
+        private RoundedButton roundedButtonSuport;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
     }
 }
 
