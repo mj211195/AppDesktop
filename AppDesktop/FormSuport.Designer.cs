90603951ac9c6147789d49350e71edd087219a7a
Juan Leon <carlos25840@hotmail.com>
2019-10-24 10:22:03 +0200

    Icon

--- AppDesktop/FormSuport.Designer.cs
+++ AppDesktop/FormSuport.Designer.cs
@@ -75,7 +75,7 @@
             this.ClientSize = new System.Drawing.Size(388, 450);
             this.Controls.Add(this.groupBoxContacto);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormSuport";
             this.Text = "Suport";
